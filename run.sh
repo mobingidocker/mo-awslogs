fadd122369f291dfa36b84fb7b7f616b1ce1f1e8
Masafumi Noguchi <ngratin@gmail.com>
2017-04-21 16:14:20 +0900

    Fix method of get environment variables

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+STACK_ID=$STACK_ID
+INSTANCE_ID=$INSTANCE_ID
+
+sed -i \
+  -e "s/MO_STACK_ID/$STACK_ID/g" \
+  -e "s/MO_INSTANCE_ID/$INSTANCE_ID/g" \
+  /etc/awslogs/awslogs.conf
+
 exec /usr/local/bin/supervisord
